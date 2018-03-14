90ca4517d1a4c04f2dd0d7cd3c68194116f6cd91
pritam-mukherjee <itfreekpm@gmail.com>
2018-03-14 15:44:28 +0530

    Bug Fixes

--- mathlab1.py
+++ mathlab1.py
@@ -1,40 +1,21 @@
-##      2+4+7-5+3
+#      2+4+7-5+3
 
-#Basic Calculator to run on Ubuntu Linux CORE
-
-def Add(nums):
-	sum = 0
-	for i in nums:
-		sum = sum + int(i)
-
-	return sum
-
-
-def Sub(nums):
-	dif = 0
-	for i in nums:
-		dif = dif - int(i)
-
-	return (sum + nums[0])
-
-
-def Bracket(math):
-	pass #Stuff to do in Bracket
+# Basic Calculator to run on Ubuntu Linux CORE
 
 
 def bracController(open, close):
-	#The Controller method to do math of stuff inside the braces [ALSO CALLED THEORY OF BRACES]
+	# The Controller method to do math of stuff inside the braces [ALSO CALLED THEORY OF BRACES]
 	open = list(reversed(open)) #Reverse the arrangements of the list
 	for i in open:
-		#Conclude(''.join(chars[int(open[i] + 1):int(close[i])]))
+		# Conclude(''.join(chars[int(open[i] + 1):int(close[i])]))
 		cmd = chars[int(open[i])+1:int(close[i])]
 		ans = Conclude(''.join(cmd))
 		chars[int(open[i]):int(close[i])+1] = str(ans)
 
 
 def divController(divIndex):
-	#The Division Handler
-	for i in mulIndex:
+	# The Division Handler
+	for i in divIndex:
 		ans = int(chars[int(i)-1]) / int(chars[int(1)+1])
 		chars[int(i)-1:int(i)+1] = ans
 
@@ -55,7 +36,7 @@ def addController(addIndex):
 
 def subController(subIndex):
 	#The Substraction Handler
-	for i in mulIndex:
+	for i in subIndex:
 		ans = int(chars[int(i)-1]) - int(chars[int(1)+1])
 		chars[int(i)-1:int(i)+1] = ans
 
@@ -79,7 +60,7 @@ def Conclude(q):
 	for x in q:
 		chars.append(x)
 
-	for i in chars:
+    for i in chars:
 		addOpIndex = [i for i, x in enumerate(chars) if x == "+"]
 		subOpIndex = [i for i, x in enumerate(chars) if x == "-"]
 		mulOpIndex = [i for i, x in enumerate(chars) if x == "*"]
@@ -87,23 +68,12 @@ def Conclude(q):
 		bracOpenIndex = [i for i, x in enumerate(chars) if x == "("]
 		bracCloseIndex = [i for i, x in enumerate(chars) if x == ")"]
 
-	bracController(bracOpenIndex, bracCloseIndex)
+    bracController(bracOpenIndex, bracCloseIndex)
 	divController(divOpIndex)
 	mulController(mulOpIndex)
 	addController(addOpIndex)
 	subController(subOpIndex)
-	return ''.join(chars)
+    return ''.join(chars)
 
 
-
-
-
-
-print(Conclude(query)) #Taking Function
-
-
-#numlist = query.split("+")
-
-
-
-#print(Add(numlist))
+print(Conclude(query)) # Taking Function
